949b3f50dc1ce5be3632386e38fd6fa70c763546
sebnow91 <sebnow91@gmail.com>
2024-04-01 13:17:41 +0200

    Finish 2nd appointment, finish 10th task in the 3rd appointment

--- adv/zad2/index.js
+++ adv/zad2/index.js
@@ -1,29 +1,73 @@
 let ID = 0;
 
 function createNewEntry() {
-  let containerDiv = document.getElementById("container");
-  let entryDiv = document.createElement("div");
-  let authorDiv = document.createElement("div");
-  let titleDiv = document.createElement("div");
-  let buttonsDiv = document.createElement("div");
+  const containerDiv = document.getElementById("container");
+  const entryDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  const buttonsDiv = document.createElement("div");
+  const authorInput = document.createElement("input");
+  const titleInput = document.createElement("input");
 
   // all ID operations have to be in this section bc later they will be updated
-  let editButton = createButton("save", ID);
-  let removeButton = createButton("remove", ID);
-  entryDiv.id = ID;
+  const editButton = createButton("save", ID, function (id) {
+    modifyEntry(this.id);
+  });
+  const removeButton = createButton("remove", ID, function (id) {
+    removeEntry(this.id);
+  });
+  entryDiv.id = "div" + ID;
+  authorDiv.id = "author" + ID;
+  titleDiv.id = "title" + ID;
   // end of ID operations section
   ID++;
 
-  authorDiv.append(document.createElement("input"));
-  titleDiv.append(document.createElement("input"));
+  authorDiv.append(authorInput);
+  titleDiv.append(titleInput);
   buttonsDiv.append(editButton, removeButton);
   entryDiv.append(authorDiv, titleDiv, buttonsDiv);
+  entryDiv.classList.add("entry");
   containerDiv.append(entryDiv);
 }
 
-function createButton(name, id) {
-  let button = document.createElement("button");
+function createButton(name, id, fn) {
+  const button = document.createElement("button");
   button.textContent = name;
   button.id = id;
+  button.onclick = fn;
+  console.log(fn);
   return button;
+}
+
+function removeEntry(id) {
+  document.getElementById("div" + id).remove();
+}
+
+function modifyEntry(id) {
+  aId = "author" + id;
+  tId = "title" + id;
+  const button = document.getElementById(id);
+  const author = document.getElementById(aId);
+  const title = document.getElementById(tId);
+  const authorChild = author.children[0];
+  const titleChild = title.children[0];
+
+  if (button.textContent === "save") {
+    button.textContent = "edit";
+    let authorText = document.createElement("div");
+    let titleText = document.createElement("div");
+    authorText.textContent = authorChild.value;
+    titleText.textContent = titleChild.value;
+
+    authorChild.remove();
+    titleChild.remove();
+    author.append(authorText);
+    title.append(titleText);
+  } else {
+    button.textContent = "save";
+    authorChild.remove();
+    titleChild.remove();
+    author.append(document.createElement("input"));
+    title.append(document.createElement("input"));
+  }
 }
