d7dc6dd8bd4859202a2396503f55e71c4c637200
sebnow91 <sebnow91@gmail.com>
2024-04-11 15:01:27 +0200

    Finish 2 assignment

---
+++ krypto/zad2/helpers.py
@@ -0,0 +1,74 @@
+import math
+import random
+import string
+
+def primesInRange(lower, upper):
+    """generate prime numbers in a given range."""
+    primes = []
+    for num in range(lower, upper):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                primes.append(num)
+    return primes
+
+
+def generateE(N):
+    primes = primesInRange(1000, 10000)
+    while True:
+        e = random.choice(primes)
+        if math.gcd(N, e) == 1:
+            return e
+
+
+def generateMessage(len):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(len))
+
+
+def encrypt(message, e, n):
+    cipher = [pow(ord(char), e, n) for char in message]
+    return cipher
+
+
+def decrypt(cipher, d, n):
+    message = [chr(pow(char, d, n)) for char in cipher]
+    return ''.join(message)
+
+
+### Diffie-Hellman ###
+
+def factor(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return set(factors)
+
+
+def is_primitive_root(g, p):
+    if pow(g, p - 1, p) != 1:
+        return False
+
+    factors = factor(p - 1)
+    for i in factors:
+        if pow(g, (p - 1) // i, p) == 1:
+            return False
+    return True
+
+
+
+def primitiveRoots(p):
+    primitive_roots = []
+    for g in range(1, p):
+        if is_primitive_root(g, p):
+            primitive_roots.append(g)
+    return primitive_roots
--- krypto/zad2/main.py
+++ krypto/zad2/main.py
@@ -1,2 +1,34 @@
+import random
+from helpers import primesInRange, generateE, generateMessage, encrypt, decrypt, primitiveRoots
+
+
 def main():
-    return 0
+    primes = primesInRange(1000, 10000)
+    p = random.choice(primes)
+    q = random.choice(primes)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = generateE(phi)
+    d = pow(e, -1, phi)
+    message = generateMessage(50)
+    print("Message:   ", message)
+    cipher = encrypt(message, e, n)
+    decrypted = decrypt(cipher, d, n)
+    print("Decrypted: ", decrypted)
+    print(message == decrypted)
+
+    # Diffie-Hellman 
+    n = random.choice(primes)
+    # g is primitive root modulo n
+    g = random.choice(primitiveRoots(n))
+    print(g, n)
+    x = random.randint(1000, 9999)
+    y = random.randint(1000, 9999)
+    X = pow(g, x, n)
+    Y = pow(g, y, n)
+    kA = pow(Y, x, n)
+    kB = pow(X, y, n)
+    print(kA, kB)
+
+
+main()
