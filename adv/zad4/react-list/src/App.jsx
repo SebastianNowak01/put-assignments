2b139288e650954aa83a10ca214943f0fe137543
sebnow91 <sebnow91@gmail.com>
2024-04-24 17:51:47 +0200

    Add zad4

--- adv/zad4/react-list/src/App.jsx
+++ adv/zad4/react-list/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
   return (
     <>
-      <h1>My awesome list!</h1>
+      <Header />
+      <List />
     </>
   );
 }
---
+++ adv/zad4/react-list/src/components/ListItem.jsx
@@ -0,0 +1,10 @@
+import data from "../assets/items.json";
+
+export default function ListItem() {
+  console.log(data);
+  return (
+    <div>
+      <h1>Items</h1>
+    </div>
+  );
+}
