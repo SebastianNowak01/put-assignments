fdc4e8c27f472dff0400e997fa6f9da4dda8cdb2
sebnow91 <sebnow91@gmail.com>
2024-05-03 18:40:44 +0200

    Update gitignore, add basic database connection

--- adv/zad5/index.js
+++ adv/zad5/index.js
@@ -4,19 +4,17 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  db.any("SELECT * FROM computer_names")
+    .then((data) => {
+      res.render("pages/index", { items: data });
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+    });
 });
 
 const pgp = require("pg-promise")(/* options */);
 const db = pgp("postgres://sebas@localhost:5432/sebas");
-
-db.one("SELECT $1 AS value", 123)
-  .then((data) => {
-    console.log("DATA:", data.value);
-  })
-  .catch((error) => {
-    console.log("ERROR:", error);
-  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
